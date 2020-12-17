033dc8f447bfb849326fdc0fab653ee3f398b3b1
Faiz Riad <54542934+faizriad94@users.noreply.github.com>
2020-12-17 17:39:45 +0800

    Added setup for odata and EF

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ExampleProject1.Data;
+using ExampleProject1.Model;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Routing.Attributes;
+
+namespace ExampleProject1.Controllers
+{
+
+    public class CustomerController : ODataController
+    {
+        private readonly DataDbContext _context;
+
+        public CustomerController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomer()
+        {
+            return await _context.Customer.ToListAsync();
+        }
+
+        [HttpGet]
+        [ODataRoute("Customer({id})")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _context.Customer.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+    }
+}
---
+++ Data/DataDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ExampleProject1.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleProject1.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) {}
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+
+    //create database using EF:
+    // - Add-Migration yourMigrationName
+    // - update-database
+}
